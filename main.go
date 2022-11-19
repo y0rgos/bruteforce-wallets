e98d653717d15f0bbab5ea3bbb9f1090e8d04c17
y0rgos <y0rgos@proton.me>
2022-11-19 21:00:56 +0200

    added go-ethereum

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 func main() {
 	fmt.Println("Hello World!")
+	url := "https://cloudflare-eth.com"
+	client, err := ethclient.Dial(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("[*] Connected to", url)
+	_ = client
 }
